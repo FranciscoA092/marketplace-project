e7849766ee4b8d3f73c004edd8773ecbb719d550
FranciscoA092 <francisco.auris@hotmail.com>
2021-12-15 08:48:57 -0300

    Create NotfoundController.php
    Adicionado controller de pagina nao encontrada.

---
+++ app/Controllers/NotfoundController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controllers;
+
+use App\Support\Page;
+
+class NotfoundController extends Page
+{
+
+    const TITLE = "Carrinho de compras";
+    
+    public function index(){
+      return $this->view('Notfound);
+    }
+    
+}
